95e2ed3b568d6f62c4748c760ff344dea719db37
Victor Mitskevich <vmitskevich@solvd.com>
2024-10-03 12:43:54 +0200

    feat: add cmd line args print method

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
+const cmdLinePrefix = '--';
+
 const parseArgs = () => {
-    // Write your code here 
+    const cmdLineArgs = process.argv.slice(2);
+    const result = cmdLineArgs.reduce((acc, item, index, arr) => {
+        if (!(index % 2)) {
+            return `${acc} ${item} is ${arr[index + 1]},`;
+        }
+
+        if (index === arr.length - 1) {
+            acc = acc.substring(0, acc.length - 1); // rm last comma
+        }
+
+        return acc;
+    }, '');
+
+    console.log(result);
 };
 
 parseArgs();
