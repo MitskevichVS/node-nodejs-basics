da9ab58037f5e61be4df5101f0e0f9a396b5ac2d
Victor Mitskevich <vmitskevich@solvd.com>
2024-10-03 10:16:07 +0200

    feat: add read method

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,35 @@
+import { existsSync, readFile } from 'node:fs';
+import { join } from 'node:path';
+
+const handleError = (customMessage) => {
+    const message = customMessage || 'FS operation failed';
+
+    throw new Error(message);
+};
+
+const readFileHandler = (err, data) => {
+    if (err) {
+        handleError();
+    }
+
+    console.log(data);
+  };
+
 const read = async () => {
-    // Write your code here 
+    const fileName = 'fileToRead.txt';
+    const currentScriptDirectory = import.meta.dirname;
+    const filesDirectory = join(currentScriptDirectory, '/files');
+    const pathToFile = join(filesDirectory, fileName);
+
+    try {
+        if (existsSync(pathToFile)) {
+            readFile(pathToFile, 'utf8', readFileHandler);
+        } else {
+            handleError();
+        }
+      } catch (err) {
+        handleError();
+      }
 };
 
 await read();
