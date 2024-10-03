a420f756e7c157d2c4a5a7f7976fe6cbbcc5bbdd
Victor Mitskevich <vmitskevich@solvd.com>
2024-10-03 09:47:25 +0200

    feat: add list method

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,33 @@
+import { readdir } from 'node:fs';
+import { join } from 'node:path';
+
+const handleError = (customMessage) => {
+    const message = customMessage || 'FS operation failed';
+
+    throw new Error(message);
+};
+
+const readFileInDir = (filesDirectory) => {
+    return new Promise((resolve, reject) => {
+        return readdir(filesDirectory, (err, filenames) => {
+            if (err) {
+                reject(err);
+            } else resolve(filenames);
+        })
+    })
+}
+
 const list = async () => {
-    // Write your code here 
+    const currentScriptDirectory = import.meta.dirname;
+    const filesDirectory = join(currentScriptDirectory, '/files');
+
+    try {
+        const files = await readFileInDir(filesDirectory);
+
+        console.log(files);
+    } catch {
+        handleError();
+    }
 };
 
 await list();
