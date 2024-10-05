5770105f0bcef1d005486bdc12a3ef06ba805e0d
Victor Mitskevich <vmitskevich@solvd.com>
2024-10-05 21:26:07 +0200

    feat: add hash calculation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,43 @@
+import { existsSync, readFile } from 'node:fs';
+import { join } from 'node:path';
+import { createHash } from 'node:crypto'
+
+const handleError = (customMessage) => {
+    const message = customMessage || 'FS operation failed';
+
+    throw new Error(message);
+};
+
+const calcHash = (content) => {
+    const result = createHash('sha256').update(content).digest('hex');
+
+    console.log(result);
+};
+
+const readFileHandler = (err, data) => {
+    if (err) {
+        handleError();
+    }
+
+    calcHash(data);
+  };
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileName = 'fileToCalculateHashFor.txt';
+    const currentScriptDirectory = import.meta.dirname;
+    const filesDirectory = join(currentScriptDirectory, '/files');
+    const pathToFile = join(filesDirectory, fileName);
+
+    try {
+        if (existsSync(pathToFile)) {
+            readFile(pathToFile, 'utf8', readFileHandler);
+        } else {
+            handleError();
+        }
+      } catch (err) {
+        handleError();
+      }
+
 };
 
 await calculateHash();
