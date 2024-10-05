ebf2dd9f46a1816e99551e68337a57c1400a1f83
Victor Mitskevich <vmitskevich@solvd.com>
2024-10-05 22:25:11 +0200

    feat: add stream read operation

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,35 @@
+import { createReadStream } from "node:fs";
+import { join } from "node:path";
+
+const handleError = (customMessage) => {
+    const message = customMessage || 'FS operation failed';
+
+    throw new Error(message);
+};
+
+const createStreamHandler = (pathToFile) => {
+    const stream = createReadStream(pathToFile, "utf8");
+
+    stream.on("error", () => {
+        handleError()
+    });
+
+    stream.on("data", (data) => {
+        process.stdout.write(data + '\n');
+    });
+
+    stream.on('end', () => {
+        console.log('stream ended');
+    });
+}
+
 const read = async () => {
-    // Write your code here 
+    const fileName = "fileToRead.txt";
+    const currentScriptDirectory = import.meta.dirname;
+    const filesDirectory = join(currentScriptDirectory, "/files");
+    const pathToFile = join(filesDirectory, fileName);
+
+    createStreamHandler(pathToFile);
 };
 
 await read();
