e0062853fbb3b39a893a5505ca6d35cf6e1b547d
Victor Mitskevich <vmitskevich@solvd.com>
2024-10-07 23:52:28 +0200

    feat: add workers methods

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { cpus } from 'node:os';
+import { Worker } from 'node:worker_threads';
+import { join } from "node:path";
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpuCount = cpus().length;
+
+    const workerThreads = [];
+
+    for (let i = 0; i < cpuCount; i++) {
+      workerThreads.push(
+        new Promise((resolve, reject) => {
+            const worker = new Worker(join(import.meta.dirname, 'worker.js'));
+
+            worker.postMessage({ number: 10 + i });
+            worker.on('message', resolve);
+            worker.on('error', reject);
+            worker.on('exit', (code) => {
+              if (code !== 0) reject(new Error(`Worker stopped with exit code ${code}`));
+            });
+        }));
+    };
+
+    Promise.allSettled(workerThreads).then((data) => {
+        const result = data.map(({status, value}) => ({data: value, status: status === 'fulfilled' ? 'resolved' : 'error'}))
+        console.log('workerThreads', result);
+    })
 };
 
 await performCalculations();
