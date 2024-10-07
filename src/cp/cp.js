e3d678df9a5212e7a00d52a61b197ab629f21a72
Victor Mitskevich <vmitskevich@solvd.com>
2024-10-08 00:22:28 +0200

    feat: add cp method

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,23 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'node:child_process';
+import { join } from 'node:path';
+
+const spawnChildProcess = async (args = []) => {
+    const fileName = 'script.js';
+    const currentScriptDirectory = import.meta.dirname;
+    const filesDirectory = join(currentScriptDirectory, '/files');
+    const scriptDirectory = join(filesDirectory, fileName);
+
+    const child = spawn('node', [scriptDirectory, ...args]);
+
+    child.stdin.write('Message from parent\n');
+
+    child.stdout.on('data', (data) => {
+        console.log(`Parent received: ${data}`);
+    });
+
+    child.on('exit', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
 };
 
 // Put your arguments in function call to test this functionality
