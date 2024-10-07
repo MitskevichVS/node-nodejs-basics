b68b9a8ff6358e04c299c98a53b61bcae0cd5c2f
Victor Mitskevich <vmitskevich@solvd.com>
2024-10-07 23:12:37 +0200

    fix: compress destination

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -5,13 +5,15 @@ import { join } from "node:path";
 
 const compress = async () => {
     const fileName = "fileToCompress.txt";
+    const archiveName = 'archive.gz';
     const currentScriptDirectory = import.meta.dirname;
     const filesDirectory = join(currentScriptDirectory, "/files");
     const pathToFile = join(filesDirectory, fileName);
+    const archiveDestination = join(currentScriptDirectory, archiveName);
 
     const gzip = createGzip();
     const source = createReadStream(pathToFile);
-    const destination = createWriteStream('archive.gz');
+    const destination = createWriteStream(archiveDestination);
 
     pipeline(source, gzip, destination, (err) => {
         if (err) {
